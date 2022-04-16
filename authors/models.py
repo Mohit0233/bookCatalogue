07812c9afc635e645e09f7ade948267a01771244
Mohit0233 <mohitkumardhiman0233@gmail.com>
2022-04-16 20:09:32 +0530

    stashed changes in model
    converted *_id foreignKey to object and added CommonInfoModel
    
    changed manytomany to foreignKey
    
    changed id to unique id
    
    changed autoInc field to char field
    
    removed unnecessary max_length
    
    altered author.phone_number as nullable
    
    fixed author_id removed on_delete
    
    changed death_date column to be nullable

--- authors/models.py
+++ authors/models.py
@@ -1,9 +1,21 @@
+import json
+
 from django.db import models
 
 
-class Author(models.Model):
-    author_id = models.AutoField(primary_key=True)
+class CommonInfoModel(models.Model):
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
+    class Meta:
+        abstract = True
+
+
+class Author(CommonInfoModel):
+    author_id = models.CharField(max_length=15, primary_key=True)
     name = models.CharField(max_length=50)
-    phone_number = models.IntegerField(max_length=20)
+    phone_number = models.IntegerField(null=True)
     birth_date = models.DateField()
-    death_date = models.DateField()
+    death_date = models.DateField(null=True)
--- books/models.py
+++ books/models.py
@@ -1,18 +1,30 @@
+import json
+
 from django.db import models
 from authors.models import Author
 
 
-class Category(models.Model):
-    category_id = models.AutoField(primary_key=True)
+class CommonInfoModel(models.Model):
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
+    class Meta:
+        abstract = True
+
+
+class Category(CommonInfoModel):
+    category_id = models.CharField(max_length=15, primary_key=True)
     category_name = models.CharField(max_length=20)
 
 
-class Book(models.Model):
-    book_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=50)
-    author_id = models.ManyToManyField(to=Author, on_delete=models.PROTECT)
+class Book(CommonInfoModel):
+    book_id = models.CharField(max_length=20, primary_key=True)
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey(to=Author, on_delete=models.PROTECT)
     publisher = models.CharField(max_length=50)
-    publish_date = models.DateField(max_length=50)
-    category_id = models.ManyToManyField(to=Category, related_name='books', related_query_name='books')
-    price = models.FloatField(max_length=10)
-    sold_count = models.IntegerField(max_length=10)
+    publish_date = models.DateField()
+    category = models.ForeignKey(to=Category, on_delete=models.PROTECT)
+    price = models.FloatField()
+    sold_count = models.IntegerField()
